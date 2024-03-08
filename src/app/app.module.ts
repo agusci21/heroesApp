e911199a741242121c253502a8747b70b4f1982e
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2024-03-08 12:03:24 -0300

    Add prime flex and Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ],
   exports: [SharedModule, SharedModule],
-  providers: [],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
